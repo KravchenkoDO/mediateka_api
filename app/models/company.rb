996b88f634cd5c246eaae454c208fb592637f5b6
KravchenkoDO <kravchenkodmo@gmail.com>
2021-07-07 07:59:07 +0300

    Add filter,pagination and sort

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,27 @@
 class Company < ApplicationRecord
+  #include Filterable
 
-  # model association
+  # model associations
   has_many :movie_companies
   has_many :movies, through: :movie_companies
+
+  # model scoups
+  scope :like, -> (key, value) { where("#{key} ILIKE ?", "%#{value}%") if value.present? }
+  scope :less_or_equal, -> (key, value) { where("#{key} <= ?", "#{value}") if value.present? }
+  scope :greater_or_equal, -> (key, value) { where("#{key} >= ?", "#{value}") if value.present? }
+  scope :pagination, -> (page, per_page) { limit(per_page).offset(per_page.to_i * (page.to_i - 1)) }
+  scope :ordering_by_params, -> (field_name, order_type) { order("#{field_name} #{order_type}") }
 
   # validation
   validates :name, uniqueness: { case_sensitive: false },
             presence: true,
             length: { in: 2..255, message: "allows string length 2..255" }
+
+  def self.filtering(params)
+    like(:name, params[:name])
+      .less_or_equal(:created_at, params[:created_at_from])
+      .greater_or_equal(:created_at, params[:created_at_to])
+      .pagination(params[:page] || 1, params[:per_page] || 10)
+      .ordering_by_params(params[:field_name] || :created_at, params[:order_type] || :desc)
+  end
 end
--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -1,23 +1,60 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Companies", type: :request do
+RSpec.describe 'Companies', type: :request do
   # initialize test data
   let(:companies) { create_list(:company, 10) }
   let(:company) { companies.first }
-
+  let(:sorted_company) { Company.order(:name).first }
+  let(:order_company) do
+    {
+      field_name: 'name',
+      order_type: 'desc'
+    }
+  end
+  let(:filtering_name) do
+    {
+      name: company.name
+    }
+  end
+  let(:pagination) do
+    {
+      page: 1,
+      per_page: 2
+    }
+  end
   # Test suite for GET /companies
-  describe "GET /companies" do
+  describe 'GET /companies' do
+    before { companies }
 
-    before do
-      companies
+    it 'returns companies' do
       get '/companies'
+      expect(parsed_body['companies'].length).to eq(10)
     end
 
-    it "returns companies" do
-      expect(parsed_body["companies"].length).to eq(10)
+    it 'returns companies' do
+      get '/companies', params: order_company
+      expect(parsed_body['companies'].last['name']).to eq(sorted_company.name)
+    end
+
+    it 'returns 1 page with 2 companies per page' do
+      get '/companies', params: pagination
+      expect(parsed_body['companies'].length).to eq(2)
+    end
+
+    it 'returns filtered companies' do
+      get '/companies', params: filtering_name
+      expect(parsed_body['companies'].length).to eq(1)
+    end
+
+    it 'returns find companies' do
+      get '/companies', params: filtering_name
+      expect(parsed_body['companies'].first['name']).to eq(company.name)
     end
 
     it 'returns status code 200' do
+      get '/companies'
       expect(response).to have_http_status(200)
     end
   end
@@ -37,8 +74,7 @@ RSpec.describe "Companies", type: :request do
     end
 
     context 'when the record does not exist' do
-
-      before { get "/companies/100" }
+      before { get '/companies/100' }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
@@ -49,13 +85,13 @@ RSpec.describe "Companies", type: :request do
   # Test suite for POST /companies
   describe 'POST /companies' do
     # valid title
-    let(:valid_attributes) { { name: 'Paramaunt Pictures' } }
+    let(:valid_attributes) { { name: 'Paramount Pictures' } }
 
     context 'when the request is valid' do
       before { post '/companies', params: valid_attributes }
 
       it 'creates a company' do
-        expect(parsed_body['name']).to eq('Paramaunt Pictures')
+        expect(parsed_body['name']).to eq('Paramount Pictures')
       end
 
       it 'returns status code 201' do
@@ -64,7 +100,7 @@ RSpec.describe "Companies", type: :request do
     end
 
     context 'when the request is invalid' do
-      before { post '/companies', params: { name: "P" } }
+      before { post '/companies', params: { name: 'P' } }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
@@ -97,28 +133,24 @@ RSpec.describe "Companies", type: :request do
     end
 
     context 'when the record does not exist' do
-
-      before { put "/companies/100" }
+      before { put '/companies/100' }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
     end
-
   end
 
   # Test suite for DELETE /companies/:id
   describe 'DELETE /companies/:id' do
-
     it 'returns status code 204' do
       delete "/companies/#{company.id}"
       expect(response).to have_http_status(204)
     end
 
     it 'returns status code 404' do
-      delete "/companies/123"
+      delete '/companies/123'
       expect(response).to have_http_status(404)
     end
   end
 end
-
