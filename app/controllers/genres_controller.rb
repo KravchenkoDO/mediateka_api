09df228b88db2bfc0ddbb2ced4fc85f23dcb27fc
SergeyAvakyan <avakyanms@gmail.com>
2021-07-12 10:50:02 +0300

    Added method permit_genre_params

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,7 @@
 class GenresController < ApplicationController
 
   before_action :find_genre, except: %i[create index]
+  before_action :permit_genre_params, only: %i[create update]
 
   def index
     @genres = Genre.filtering(params)
@@ -9,14 +10,14 @@ class GenresController < ApplicationController
   def show; end
 
   def create
-    @genre = Genre.new(title: params[:title])
+    @genre = Genre.new(@permitted_params)
     return render json: { errors: @genre.errors.messages }, status: 422 unless @genre.save
 
     render status: :created
   end
 
   def update
-    unless @genre.update(title: params[:title])
+    unless @genre.update(@permitted_params)
       render json: { errors: @genre.errors.messages }, status: :unprocessable_entity
     end
   end
@@ -30,5 +31,9 @@ class GenresController < ApplicationController
   def find_genre
     @genre = Genre.find_by_id params[:id]
     head :not_found unless @genre
+  end
+
+  def permit_genre_params
+    @permitted_params = params.require(:genre).permit(:title)
   end
 end
